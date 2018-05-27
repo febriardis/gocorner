69a62d22ac447d6fd3feb53f910992aca1abaea1
febriardis <ardisaputrafebri@gmail.com>
2018-05-27 09:20:49 +0700

    edit-view7

---
+++ resources/views/warung.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.layout')
+
+@section('content')
+	<!-- Page container -->
+			<div class="col-sm-4 col-lg-2">
+				<div class="panel">
+					<img src="assets/images/placeholder.jpg" style="width: 100%; max-height: 150px" alt="">
+					<div class="p-15">
+						<div class="media-body">
+							<strong>WARUNG CEU ENTIN</strong>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-4 col-lg-2">
+				<div class="panel">
+					<img src="assets/images/placeholder.jpg" style="width: 100%; max-height: 150px" alt="">
+					<div class="p-15">
+						<div class="media-body">
+							<strong>WARUNG KANG IDOY</strong>
+						</div>
+					</div>
+				</div>
+			</div>
+	<!-- /page container -->
+@endsection
